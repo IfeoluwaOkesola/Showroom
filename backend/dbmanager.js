1a6a3aad803944f6fc013fa3c02f6e9791de46df
IfeoluwaOkesola <atoyebiife@gmail.com>
2023-01-14 21:54:18 +0100

    uncomment dbmanager

--- backend/dbmanager.js
+++ backend/dbmanager.js
@@ -1,9 +1,8 @@
 const sequelize = require("./config/connections");
-//const { Statement } = require("./models/accountStatement");
 const Transaction = require("./models/transactions");
-// const { Customer } = require("./models/customer");
-//const { deposit } = require("./models/savings");
-//const { withdrawal } = require("./models/withdrawal");
+ const { Customer } = require("./models/customer");
+const { deposit } = require("./models/savings");
+const { withdrawal } = require("./models/withdrawal");
 
 sequelize.sync({force:true}).then(rs=>{
     console.log(rs)
